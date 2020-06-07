fc5e15327314debbbcaa555b5174bbcf9a2dc680
Ryosuke <ryosuke.san.hana@gmail.com>
2020-06-07 00:26:59 -0700

    :wrench: Added Preact plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Preact Default Starter`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
   },
   plugins: [
+    `gatsby-plugin-preact`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
